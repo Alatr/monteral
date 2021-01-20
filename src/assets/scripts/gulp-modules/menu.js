3b10c596944d3c2bef1583f2ee373fbb66f9ebfe
Dron2019 <mr.andrey@bigmir.net>
2021-01-20 16:01:07 +0200

    first dropdown menu opened by default

--- src/assets/scripts/gulp-modules/menu.js
+++ src/assets/scripts/gulp-modules/menu.js
@@ -1,7 +1,7 @@
-$menu = document.querySelector('.menu');
-$menuImage = document.querySelector('.js-menu-img');
-$menuTextBlock = document.querySelector('.menu__text-wrap');
-$menuInnerLinks = document.querySelectorAll('.menu__inner-links');
+let $menu = document.querySelector('.menu');
+let $menuImage = document.querySelector('.js-menu-img');
+let $menuTextBlock = document.querySelector('.menu__text-wrap');
+let $menuInnerLinks = document.querySelectorAll('.menu__inner-links');
 $menuInnerLinks.forEach(link => {
     link.querySelector('*') ? link.previousElementSibling.classList.add(link.dataset.hasChildClass) : null;
 })
@@ -27,5 +27,18 @@ function switchMenuImage(evt) {
 }
 $menuImage.addEventListener('load', function(evt) {
     $menuImage.style.opacity = 1;
-    $menuImage.style.animation = `img-in .5s ease-out`;
+    $menuImage.style.animation = `img-in 1s ease-out`;
 });
+
+
+/** */
+const classForHoverLink = 'hover';
+let $pointsWithChild = document.querySelectorAll('.has-child');
+/** */
+$pointsWithChild[0].classList.add(classForHoverLink);
+$pointsWithChild.forEach(el => {
+    el.addEventListener('mouseenter', function(evt) {
+        $menu.querySelector(`.${classForHoverLink}`).classList.remove(classForHoverLink);
+        el.classList.add(classForHoverLink);
+    });
+})
