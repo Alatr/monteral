5a2c3f23630c8d541ff0f74c10c392cbdcb112b3
Dron2019 <mr.andrey@bigmir.net>
2021-01-20 15:20:28 +0200

    mobile menu text sizes add

--- src/assets/scripts/gulp-modules/menu.js
+++ src/assets/scripts/gulp-modules/menu.js
@@ -1,15 +1,31 @@
 $menu = document.querySelector('.menu');
 $menuImage = document.querySelector('.js-menu-img');
-
+$menuTextBlock = document.querySelector('.menu__text-wrap');
 $menuInnerLinks = document.querySelectorAll('.menu__inner-links');
 $menuInnerLinks.forEach(link => {
-
     link.querySelector('*') ? link.previousElementSibling.classList.add(link.dataset.hasChildClass) : null;
 })
 
 document.querySelectorAll('[data-image]').forEach(el => {
-    el.addEventListener('mouseenter', function(evt) {
-        $menuImage.src = el.dataset.image;
-        $menuImage.style.filter = `blur(${el.dataset.blur}px)`;
-    });
-})
+    el.addEventListener('mouseenter', switchMenuImage);
+});
+
+
+$menuTextBlock.addEventListener('mouseleave', function(evt) {
+    // $menuImage.style.opacity = 0;
+});
+
+function switchMenuImage(evt) {
+    let linkWithImage = evt.target;
+
+    if ($menuImage.dataset.image === linkWithImage.dataset.image) return;
+    $menuImage.style.animation = `img-out .5s ease-out`;
+    setTimeout(() => {
+        $menuImage.dataset.image = linkWithImage.dataset.image;
+        $menuImage.src = linkWithImage.dataset.image;
+    }, 500);
+}
+$menuImage.addEventListener('load', function(evt) {
+    $menuImage.style.opacity = 1;
+    $menuImage.style.animation = `img-in .5s ease-out`;
+});
