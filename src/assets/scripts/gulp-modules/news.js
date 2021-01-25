3cdac24e6ae755caa53f5be5a90105b2103e8950
Dron2019 <mr.andrey@bigmir.net>
2021-01-25 14:27:52 +0200

    NewsLinkAdd,paginationBlockAdd

--- src/assets/scripts/gulp-modules/news.js
+++ src/assets/scripts/gulp-modules/news.js
@@ -36,3 +36,33 @@ window.addEventListener('load', function(evt) {
         gsap.from(el, { autoAlpha: 0 })
     })
 });
+
+
+// document.querySelectorAll('[data-repaint-on-hover]').forEach(colorMe)
+
+// function colorMe(element) {
+//     element.addEventListener('mouseenter', function(evt) {
+//         let pathId = element.querySelector('use').getAttribute('xlink:href');
+
+//         document.querySelectorAll(`${pathId} [fill]`).forEach(el => {
+//             el.defaultColor = getComputedStyle(el).fill;
+//             el.style.fill = 'red';
+
+//         })
+//         document.querySelectorAll(`${pathId} [stroke]`).forEach(el => {
+//             el.defaultColor = getComputedStyle(el).stroke;
+//             el.style.stroke = 'red';
+//         })
+//     });
+//     element.addEventListener('mouseleave', function(evt) {
+//         let pathId = element.querySelector('use').getAttribute('xlink:href');
+//         document.querySelectorAll(`${pathId} [fill]`).forEach(el => {
+//             console.log(el.defaultColor);
+//             el.style.fill = el.defaultColor;
+//         })
+//         document.querySelectorAll(`${pathId} [stroke]`).forEach(el => {
+//             console.log(el.defaultColor);
+//             el.style.stroke = el.defaultColor;
+//         })
+//     });
+// }
