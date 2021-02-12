d4b8b37aeeb3d52c2281aa1675b1e3b24664789d
Dron2019 <mr.andrey@bigmir.net>
2021-02-12 13:30:18 +0200

    firstScreenCrossScreenANim

--- src/assets/scripts/gulp-modules/smooth_scroll.js
+++ src/assets/scripts/gulp-modules/smooth_scroll.js
@@ -246,19 +246,58 @@ const niceDuration = 3;
  */
 /**block2 people transform length */
 
-document.querySelectorAll('.page-part,.page-first-block,.page__footer-wrapper').forEach(el => el.setAttribute('data-scroll-section', ''))
+document.querySelectorAll('.page-part,.page__footer-wrapper').forEach(el => el.setAttribute('data-scroll-section', ''))
 const header = document.querySelector('.header');
 const locoScroll = new LocomotiveScroll({
     el: document.querySelector('.page__inner'),
     smooth: true,
     smoothMobile: false,
-    inertia: 1.1,
+    inertia: 1,
+    lerp: 0.2,
     getDirection: true,
     resetNativeScroll: false,
     onUpdate: function(some) {
 
     }
 });
+stopScrollAndDetectIt(locoScroll);
+window.addEventListener('wheel', firstScrollAnimAndEnableSmooth, { passive: false });
+setInterval(() => {
+    console.log(locoScroll.isStopped);
+}, 100);
+window.addEventListener('load', function(evt) {
+    let tl = new gsap.timeline();
+    tl.fromTo('.page-first-block__text-wrap>*', { y: 100, autoAlpha: 0 }, { y: 0, autoAlpha: 1, stagger: 0.1 })
+});
+
+function firstScrollAnimAndEnableSmooth(evt) {
+    evt.preventDefault();
+    window.removeEventListener('wheel', firstScrollAnimAndEnableSmooth);
+    stopScrollAndDetectIt(locoScroll)
+    if (locoScroll.isStopped) {
+        console.log('fff');
+        firstSecondTransferAnimation(() => {
+            startScrollAndDetectIt(locoScroll);
+            document.querySelector('.page-first-block').setAttribute('data-scroll-section', '');
+            locoScroll.update();
+
+        });
+
+    }
+}
+
+function stopScrollAndDetectIt(instance) {
+    instance.stop();
+    instance.isStopped = true;
+}
+
+function startScrollAndDetectIt(instance) {
+    instance.start();
+    instance.isStopped = false;
+
+}
+
+
 document.querySelectorAll('.up-arrow').forEach(arrow => {
     document.body.append(arrow)
     arrow.addEventListener('click', function(evt) {
@@ -289,7 +328,7 @@ locoScroll.on("scroll", (position, limit, speed, direction) => {
             [document.querySelector('.up-arrow'), 'headroom--not-top'],
         ]);
 });
-// locoScroll.on('update', (some) => { console.log(some); })
+
 ScrollTrigger.scrollerProxy(document.body, {
     scrollTop(value) {
         return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
@@ -311,37 +350,33 @@ const firstBlock = document.querySelectorAll('.page-first-block');
 const niceImages = document.querySelectorAll('[data-nice-entry]');
 const doublePartImages = document.querySelectorAll('[data-double-part-block]');
 const niceBigImages = document.querySelectorAll('[data-nice-entry-big]');
+const pageBlockTitles = document.querySelectorAll('.page-block-title,.block-with-decor2__count,.block-with-decor2__text')
 const amplitude = document.documentElement.clientWidth < 576 ? 40 : 80;
 
-firstBlock.forEach((big) => {
+pageBlockTitles.forEach((big, index) => {
+
+    gsap.set(big, { autoAlpha: 0 })
     ScrollTrigger.create({
         trigger: big,
-        start: "0.75",
-        // endTrigger: ".main-screen-slider",
-        markers: true,
+        offset: '0.25',
         end: "bottom",
-        onUpdate: self => {
-            gsap.to('canvas, .page-first-block__bg', { y: self.progress * (amplitude * 2), duration: 0.5 });
-            gsap.to('canvas, .page-first-block__bg', { scale: 1 + (self.progress / 10 / 2 / 2), duration: 0.5 });
+        onEnter: self => {
+            if (big.isInvoked === undefined) {
+                gsap.fromTo(big, { y: 50, autoAlpha: 0, }, { autoAlpha: 1, y: 0, duration: 0.75, delay: index < 5 ? index / 10 : index / 20, ease: Power3.easeOut, stagger: 0.2 });
+                big.isInvoked = true;
+            }
         },
-
     });
-
 });
 niceBigImages.forEach((big) => {
     ScrollTrigger.create({
         trigger: big,
-        // start: "top",
-        // endTrigger: ".main-screen-slider",
-        /*markers: true, */
         end: "bottom",
         onUpdate: self => {
             gsap.to(big.querySelector('.block-with-logo-decor img'), { y: amplitude / -2 + self.progress * amplitude });
-            gsap.to(big.querySelector('.block-with-logo-decor .gradient-bg'), { y: amplitude / -2 + self.progress * amplitude });
+            // gsap.to(big.querySelector('.block-with-logo-decor .gradient-bg'), { y: amplitude / -2 + self.progress * amplitude });
         },
-
     });
-
 });
 
 doublePartImages.forEach(paralaxImg => {
@@ -357,18 +392,15 @@ doublePartImages.forEach(paralaxImg => {
     gsap.set(textToAnimate, { clipPath: `polygon(0px 0px, 100% 0px, 100% 0%, 0px 0%)`, });
     ScrollTrigger.create({
         trigger: paralaxImg,
-        // start: "top",
-        // endTrigger: ".main-screen-slider",
-        /*markers: true, */
         end: "bottom",
         onEnter: self => {
             if (!imgToAnimate.cliPathed) {
-                gsap.to(imgToAnimate, { clipPath: `polygon(0 0, 100% 0, 100% 100%, 0 100%)`, ease: niceBezier, duration: 0.5 });
+                gsap.to(imgToAnimate, { clipPath: `polygon(0 0, 100% 0, 100% 100%, 0 100%)`, ease: niceBezier, duration: 1 });
                 imgToAnimate.cliPathed = true;
             }
             if (!textToAnimate.isClipPathed) {
 
-                gsap.to(textToAnimate, { clipPath: `polygon(0 0, 100% 0, 100% 100%, 0 100%)`, stagger: 0.2, ease: niceBezier, duration: 0.25 });
+                gsap.to(textToAnimate, { clipPath: `polygon(0 0, 100% 0, 100% 100%, 0 100%)`, stagger: 0.2, ease: niceBezier, duration: 1 });
                 textToAnimate.isClipPathed = true;
             }
         },
@@ -387,27 +419,18 @@ niceImages.forEach(paralaxImg => {
     let height = imgToAnimate.getBoundingClientRect().height;
     let scaleCoef = (height + (amplitude)) / height;
     gsap.set(imgToAnimate, { scale: scaleCoef })
-        // gsap.set(paralaxImg.closest('.block-with-decor2'), { y: amplitude })
-    gsap.set(imgToAnimate, { clipPath: `polygon(0px 0px, 100% 0px, 100% 0%, 0px 0%)`, ease: niceBezier, duration: 1 });
+        // gsap.set(imgToAnimate, { clipPath: `polygon(0px 0px, 100% 0px, 100% 0%, 0px 0%)`, ease: niceBezier, duration: 1 });
+    gsap.set(imgToAnimate, { transformOrigin: `top right`, x: 50 });
     ScrollTrigger.create({
-        trigger: paralaxImg,
+        trigger: imgToAnimate,
         /*markers: true, */
         end: "bottom",
         onEnter: self => {
             if (!imgToAnimate.cliPathed) {
-                gsap.to(imgToAnimate, { clipPath: `polygon(0 0, 100% 0, 100% 100%, 0 100%)`, ease: niceBezier, duration: 1 });
+                // gsap.to(imgToAnimate, { clipPath: `polygon(0 0, 100% 0, 100% 100%, 0 100%)`, ease: niceBezier, duration: 1 });
+                gsap.to(imgToAnimate, { rotate: 0, ease: niceBezier, duration: 2 });
                 imgToAnimate.cliPathed = true;
             }
-            // gsap.to(paralaxImg.closest('.block-with-decor2'), { y: 0, duration: 2 })
-        },
-        onLeave: self => {
-            // gsap.to(paralaxImg.closest('.block-with-decor2'), { y: amplitude, easing: niceBezier, })
-        },
-        onLeaveBack: self => {
-            // gsap.to(paralaxImg.closest('.block-with-decor2'), { y: amplitude, easing: niceBezier, })
-        },
-        onEnterBack: self => {
-            // gsap.to(paralaxImg.closest('.block-with-decor2'), { y: 0, easing: niceBezier, })
         },
         onUpdate: self => gsap.to(imgToAnimate, { y: amplitude / -2 + self.progress * amplitude }),
     });
@@ -443,3 +466,23 @@ ScrollTrigger.refresh();
  * LOCOMOTIVE SCROLL end
  */
 /**********************************/
+
+
+document.querySelector('.screen-icon-scroll').addEventListener('click', function(evt) {
+    firstSecondTransferAnimation()
+});
+const scrollEasing = new BezierEasing(.14, .76, .63, .99);
+
+function firstSecondTransferAnimation(callback = () => {}) {
+    let tl = gsap.timeline();
+    tl.set(document.querySelectorAll('.page-part:nth-child(2)>*'), { autoAlpha: 0.5, duration: 0.5 });
+    tl.to(document.querySelectorAll('.page-first-block__text-wrap>*'), { y: -150, autoAlpha: 0, stagger: 0.1, ease: Power3.easeIn, duration: 0.75 })
+    tl.to('canvas, .page-first-block__bg', { scale: 1.08, ease: scrollEasing, duration: 2.3 }, '<0.5');
+    // tl.to('', { y: -150, autoAlpha: 0, stagger: 0.1, ease: Power3.easeIn, duration: 0.75 },'<')
+    tl.add(() => { locoScroll.scrollTo(document.documentElement.clientHeight) }, '<');
+    tl.fromTo(document.querySelector('.page-part:nth-child(2)>*'), { autoAlpha: 0, y: -100 }, { autoAlpha: 1, y: 0, stagger: 0.35, duration: 1.5, ease: Power4.easeOut }, '<');
+    tl.set(document.querySelectorAll('.page-first-block__text-wrap>*'), { y: 0, autoAlpha: 1, });
+    tl.add(callback);
+    tl = '';
+
+}
